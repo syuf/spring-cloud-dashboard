62765d6b7d30310446fb6c5446a81467fdb3d1ff
shiyuanfei <356418670@qq.com>
2018-06-28 17:17:25 +0800

    初始化项目

---
+++ spring-cloud-dashboard/src/main/java/com/zlpay/SpringCloudDashboardApplication.java
@@ -0,0 +1,16 @@
+package com.zlpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@SpringBootApplication
+public class SpringCloudDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudDashboardApplication.class, args);
+	}
+}
